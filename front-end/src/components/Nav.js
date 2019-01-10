7f892937be17fd09992471ef184818c6a79dd393
Joseph Arias <arias.joseph.a@gmail.com>
2019-01-10 11:08:38 -0500

    Integraded auser navigation bar when logged in and a guest navigation bar when user is not signed in

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { NavLink, withRouter } from "react-router-dom";
-import { Menu } from "semantic-ui-react";
+import { Menu, Icon, Label, Input  } from "semantic-ui-react";
 
 const Nav = ({location: { pathname}, logged_in, setCurrentUser}) => {
   
@@ -11,11 +11,13 @@ const Nav = ({location: { pathname}, logged_in, setCurrentUser}) => {
 
   return (
     <Menu className="navbar" pointing secondary size="huge">
+      {logged_in ? (
       <Fragment>
         <Menu.Item
           as={NavLink}
           to="/home"
           name="SkillHub"
+          className="logo"
           active={pathname === "/home"}
         />
         <Menu.Item
@@ -28,22 +30,51 @@ const Nav = ({location: { pathname}, logged_in, setCurrentUser}) => {
           as={NavLink}
           to="/myProperties"
         />
-      
         <Menu.Menu position="right">
-        <Menu.Item
-          name='Inbox'
-          as={NavLink}
-          to="/messages"
-        />
+        <Menu.Item as='a'>
+          <Icon name='mail' />Messages
+          <Label color='red'>
+            7
+          </Label>
+        </Menu.Item>
           <Menu.Item to="/logout" name="Logout" onClick={logout} />
         </Menu.Menu>
       </Fragment>
-      <Menu.Item
-        as={NavLink}
-        to="/login"
-        name="Login"
-        active={pathname === "/login"}
-      />
+      ) : (
+        <Fragment>
+          <Menu.Item
+          as={NavLink}
+          to="/home"
+          name="SkillHub"
+          className="logo"
+          active={pathname === "/home"}
+          />
+          <Menu.Item
+          name='Explore'
+          as={NavLink}
+          to="/explore"
+          />
+          <Menu.Item>
+            <Input size='small' className='icon' icon='search' placeholder='Search...' />
+          </Menu.Item>
+          
+          <Menu.Menu position="right">
+          <Menu.Item
+            name='Become a Mentor'
+            as={NavLink}
+            to="/myProperties"
+          />
+          <Menu.Item
+            as={NavLink}
+            to="/login"
+            name="Login"
+            className="login-btn"
+            active={pathname === "/login"}
+          />
+          </Menu.Menu>
+        </Fragment>
+      )}
+      
     </Menu>
   )
 }
--- front-end/src/containers/Home.js
+++ front-end/src/containers/Home.js
@@ -1,22 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import { Button } from 'semantic-ui-react'
-
-
 class Home extends Component {
   render() {
     return (
       <Fragment>
-        {this.props.currentUser ? (
           <div className="jumbo-container">
             <div className="jumbo-catchphase">
-              <h1>Meet up and Learn from a Mentor</h1>
+              <h1>Meet up and Learn a Skill from a Mentor</h1>
               <h2>Find a local mentor and accelerate your learning</h2>
                 <div className="get-started-container">
                 <Button className="get-started-btn" color='teal'>Get Started</Button>
                 </div>
               </div>
           </div>
-        ) : null }
+        
       </Fragment>
     )
   }
