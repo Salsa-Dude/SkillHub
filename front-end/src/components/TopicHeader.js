c4d2f1e16dbed38e7542941fa14d3a7fe1a9fe62
Joseph Arias <arias.joseph.a@gmail.com>
2019-01-14 21:34:25 -0500

    Change landing page jumbo image

--- front-end/src/components/TopicHeader.js
+++ front-end/src/components/TopicHeader.js
@@ -1,5 +1,8 @@
 import React, {Fragment} from 'react'
-import { Grid, GridColumn } from 'semantic-ui-react'
+import { NavLink} from 'react-router-dom'
+import { Grid, GridColumn, Breadcrumb } from 'semantic-ui-react'
+
+
 
 import '../styles/topicHeader.css'
 
@@ -7,17 +10,19 @@ import '../styles/topicHeader.css'
 const TopicHeader = (props) => {
 
   const topicHeaderBackground = {
-    backgroundImage: `linear-gradient(rgba(100, 99, 99, 0.7), rgba(100, 99, 99, 0.7)), url(${props.img})`,
+    backgroundImage: `linear-gradient(rgba(100, 99, 99, 0.5), rgba(100, 99, 99, 0.5)), url(${props.img})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     backgroundPosition: 'center'
   }
 
   const exploreGrid = {
-  
     marginLeft: "10%",
     marginTop: "20px",
-    border: 'solid'
+  }
+
+  const breadCrumb = {
+    color: 'white'
   }
 
   return (
@@ -25,6 +30,11 @@ const TopicHeader = (props) => {
       <div style={topicHeaderBackground} className="topic-header-container">
         <Grid>
           <Grid.Column style={exploreGrid} width={8}>
+          <Breadcrumb size='big'>
+            <Breadcrumb.Section style={breadCrumb} as={NavLink} to="/explore" >Explore</Breadcrumb.Section>
+            <Breadcrumb.Divider style={breadCrumb} icon='right chevron' />
+            <Breadcrumb.Section active>Dancing</Breadcrumb.Section>
+          </Breadcrumb>
             <h1>{props.title}</h1>
           </Grid.Column>
         </Grid>
